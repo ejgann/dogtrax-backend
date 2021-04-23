86fbd4c61688786678bebef29cbcbcf770a0b01f
Emily Gann <aspentree79@gmail.com>
2021-04-22 21:13:49 -0400

    built owners controller, created api and v1 folders for controllers

---
+++ app/controllers/api/v1/owners_controller.rb
@@ -0,0 +1,37 @@
+class Api::V1::OwnersController < ApplicationController
+
+    def show
+        owner = Owner.find(params[:id])
+        render json: owner
+    end
+
+    def create
+        owner = Owner.new(owner_params)
+        if owner.save
+            render json: owner 
+        else
+            render error: {error: "Unable to create owner."}
+        end
+    end
+
+    def update
+        owner = Owner.find(params[:id])
+        if owner
+            owner.update(owner_params)
+        else
+            render error: {error: "Unable to update owner."}
+        end
+    end
+
+    def destroy
+        owner = Owner.find(params[:id])
+        owner.destroy
+    end
+
+    private
+
+    def owner_params
+        params.require(:owner).permit(:name, :email, :phone)
+    end
+
+end
