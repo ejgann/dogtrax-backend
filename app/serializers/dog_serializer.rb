07ebcc476ec68d0963e37490368c1ffe5e5f9946
Emily Gann <aspentree79@gmail.com>
2021-05-02 16:56:44 -0400

    added custom serializer to dog serializer to bring in owner info to render with dog info

--- app/serializers/dog_serializer.rb
+++ app/serializers/dog_serializer.rb
@@ -1,5 +1,11 @@
 class DogSerializer < ActiveModel::Serializer
-  attributes :id, :name, :age, :gender, :breed, :notes, :owner_id
+  attributes :id, :name, :age, :gender, :breed, :notes, :ownerInfo
 
   has_many :reports
+
+  def ownerInfo
+    combined_info = object.owner.name + ', ' + object.owner.phone + ', ' + object.owner.email
+    return combined_info
+  end
+
 end
